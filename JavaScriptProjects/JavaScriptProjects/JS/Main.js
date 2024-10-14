4038e527b850ec548589f44d8b9f12a023158e01
irwinc27 <irwinc27@gmail.com>
2024-10-14 13:10:05 +0100

    Add files via upload

---
+++ JavaScriptProjects/JavaScriptProjects/JS/Main.js
@@ -0,0 +1,14 @@
+ var x = "Hello,World!"; //This is a variable
+ document.write (x); //Displays the value of "x"
+
+ window.alert("Hello,World!") //Pops up the alert of "Hello,World!" when load website
+
+ var Family= "Skywalker", Dad = "Anakin", Son = "Luke", Daughter = "leia";
+ document.write(Dad); //Displays the value of the element "Dad"
+
+ var Sent1 = "This is the begining of the string. ";
+ var Sent2 = "This the end of the string"; //These are two variables
+
+ document.write (Sent1 + Sent2) //Displays the two variables together
+
+ 8+8 //This is an expression
